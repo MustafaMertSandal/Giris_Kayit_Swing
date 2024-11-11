51266a8ecb0b7a595497bab542b4ac6f0b7c2cd1
MustafaMertSandal <mmert.sandal@gmail.com>
2024-11-11 17:28:08 +0300

    Veritabanina kisi ekleme ozelligi eklendi.

--- src/VeriTabani.java
+++ src/VeriTabani.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class VeriTabani {
     private static final String URL = "jdbc:postgresql://localhost:5432/giriskayit";
@@ -23,8 +20,8 @@ public class VeriTabani {
         String SQL = "CREATE TABLE IF NOT EXISTS members (" +
                 "id SERIAL PRIMARY KEY, " +
                 "name VARCHAR(100) NOT NULL, " +
-                "nickname VARCHAR(100) NOT NULL, " +
-                "email VARCHAR(100) NOT NULL, " +
+                "nickname VARCHAR(100) NOT NULL UNIQUE, " +
+                "email VARCHAR(100) NOT NULL UNIQUE, " +
                 "password VARCHAR(100) NOT NULL, " +
                 "phone VARCHAR(12) NOT NULL, " +
                 "photo BYTEA" +
@@ -37,6 +34,26 @@ public class VeriTabani {
 
         } catch (SQLException e) {
             System.out.println("Tablo oluşturulurken hata oluştu: " + e.getMessage());
+        }
+    }
+
+    public void addMember(String name, String nickName, String email, String password, String phone, byte[] photo){
+        String SQL = "INSERT INTO members (name, nickname, email, password, phone, photo) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, name);
+            pstmt.setString(2, nickName);
+            pstmt.setString(3, email);
+            pstmt.setString(4, password);
+            pstmt.setString(5, phone);
+            pstmt.setBytes(6, photo);
+            pstmt.executeUpdate();
+
+            System.out.println("Veri eklendi!");
+
+        } catch (SQLException e) {
+            System.out.println("Veri ekleme hatası: " + e.getMessage());
         }
     }
 }
